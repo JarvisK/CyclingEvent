46949483b052c88cfdfbfc5eeab823e449b6a07a
Jarvis <koeicoei@gmail.com>
2017-04-19 00:13:03 +0800

    Complete Taiwanbike crawler

--- crawler.py
+++ crawler.py
@@ -1,25 +1,51 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+
 class TaiwanBikeCrawler():
     domainURL = "http://www.taiwanbike.org"
     targetURL = "http://www.taiwanbike.org/index.php/2009-01-20-17-17-03"
+    pageSeparator = 20
+    eventName = []
+    eventURL = []
 
-    def getHtmlSourceCode(self):
-        with urllib.request.urlopen(self.targetURL) as response:
-            self.sourceCode = response.read()
-            self.parser()
+    def startCrawler(self):
+        keep = True
+        fetchURL = self.targetURL
 
-    def parser(self):
-        soup = BeautifulSoup(self.sourceCode, "lxml")
-        tableRow = soup.find('form', attrs={'name':'adminForm'}).find('table').find_all('tr')
+        while (keep):
+            with urllib.request.urlopen(fetchURL) as response:
+                source = response.read()  # Get response source code
 
-        eventName = []
-        eventURL = []
+                if source is None:
+                    raise ValueError("The source code is empty.")
+
+                if (not self.parser(sourceCode=source)):
+                    keep = False
+                else:
+                    fetchURL = self.targetURL + "?start=" + str(self.pageSeparator)
+                    self.pageSeparator += 20
+
+        print("Crawler success.")
+
+    def parser(self, sourceCode=None):
+        if sourceCode is None:
+            raise ValueError("The source code is empty.")
+
+        print("Processing " + self.targetURL + "...")
+
+        soup = BeautifulSoup(sourceCode, "lxml")
+        tableRow = soup.find('form', attrs={'name': 'adminForm'}).find('table').find_all('tr')
+
+        if tableRow == None:
+            return False
 
         for i, row in enumerate(tableRow):
-            if 3 < i < 23:
-                eventName.append(str(row.contents[3].a.string).strip('\t\n\r'))
-                eventURL.append(str(row.contents[3].a.attrs['href']).strip('\t\n\r'))
+            if 2 < i < 23:
+                try:
+                    self.eventName.append(str(row.contents[3].a.string).strip('\t\n\r'))
+                    self.eventURL.append(self.domainURL + str(row.contents[3].a.attrs['href']).strip('\t\n\r'))
+                except:
+                    return False
 
-        print(eventURL)
+        return True
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from crawler import TaiwanBikeCrawler
 
 def main():
     a = TaiwanBikeCrawler()
-    a.getHtmlSourceCode()
+    a.startCrawler()
+    # http://bao-ming.com/eb/www/reg.php?activitysn=2048
 
 if __name__ == "__main__":
     main()
